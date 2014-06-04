5e40827a662955ea1ecfe5ed432e64c13dff23e5
Joakim Svensson <joakimsv@axis.com>
2014-06-04 09:29:40 +0200

    First version of the app

---
+++ server.js
@@ -0,0 +1,54 @@
+var request = require('request'),
+	cheerio = require('cheerio'),
+	express = require('express'),
+	app = express(),
+	server,
+	headers = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag'],
+	edison_options = {
+		uri: 'http://restaurangedison.se/Bizpart.aspx?tabId=191'
+	};
+
+// Add a proxy if http_proxy is set in the environment variables
+if (process.env.http_proxy !== undefined) {
+	edison_options.proxy = process.env.http_proxy;
+}
+
+// Static files (.html, .css, .js et c)
+app.use(express.static(__dirname));
+
+// Respond with the index.html file if root is requested 
+app.get('/', function(req, res) {
+	res.sendfile('index.html');
+});
+
+app.get('/get_lunches', function(req, res) {
+	request(edison_options, function (error, response, body) {
+		var	finalLunches = [],
+			$ = cheerio.load(body);
+
+			$('#MyBPControlLayout_Container_535_divContainer p').eq(4).contents().filter(function(i, el) {
+				// Filter out everything that is not text elements that actually has text
+				return el.type === 'text' && el.data.trim() !== '';
+			}).each(function(i, el) {
+				var current = el.data.trim(),
+					// Get the first word of the current element (May be "Fredag STÄNGT")
+					currentFirst = current.split(' ')[0],
+					header = 0;
+
+				// Check if this word should be considered a header
+				if (headers.indexOf(currentFirst) >= 0) {
+					header = 1;
+				}
+
+				finalLunches.push({header: header, text: current});
+			});
+
+		// Send the response
+		res.send(finalLunches);
+	});
+});
+
+// Start the server
+server = app.listen(3000, function() {
+	console.log('Listening on port %d', server.address().port);
+});
