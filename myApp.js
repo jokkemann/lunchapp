7d42d4a8f9d10d512403244b43d96092a514c5bd
Joakim Svensson <svensson.joakim@gmail.com>
2015-04-13 21:58:28 +0200

    Reflect Edison page changes on the site

--- myApp.js
+++ myApp.js
@@ -3,4 +3,13 @@ controller('LunchCtrl', function($scope, $http) {
 	$http.get('get_lunches').success(function(it) {
 		$scope.lunchItems = it;
 	});
+	$scope.isArray = function (v) {
+		if (!Array.isArray){
+			Array.isArray = function(arg) {
+				return Object.prototype.toString.call(arg) === '[object Array]';
+			};
+		}
+
+		return Array.isArray(v);
+	}
 });
