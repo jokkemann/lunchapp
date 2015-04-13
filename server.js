084ff469f7e20a3425854c177fccf97e76d0dcdc
Joakim Svensson <svensson.joakim@gmail.com>
2015-04-13 21:37:38 +0200

    Updated server to reflect new Edison page

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var request = require('request'),
 	server,
 	headers = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag'],
 	edison_options = {
-		uri: 'http://restaurangedison.se/Bizpart.aspx?tabId=191'
+		uri: 'http://restaurangedison.se/lunch'
 	};
 
 // Add a proxy if http_proxy is set in the environment variables
@@ -21,35 +21,47 @@ app.get('/', function(req, res) {
 	res.sendfile('index.html');
 });
 
-app.get('/get_lunches', function(req, res) {
-	request(edison_options, function (error, response, body) {
-		var	finalLunches = [],
-			$ = cheerio.load(body),
-			baseParagraph = $('#MyBPControlLayout_Container_535_divContainer > span p'),
-			paragraph = baseParagraph.eq(0),
-			textNodes = paragraph.contents().filter(function(i, el) {
-				// Filter out everything that is not text elements that actually has text
-				return el.type === 'text' && el.data.trim() !== '';
+var parseLunches = function(body) {
+	var weeklyInfo = [],
+		$ = cheerio.load(body),
+		days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'],
+		weekElem = $('.week_number').first(); //This one is not needed yet
+
+	days.forEach(function(e, i) {
+		// Get all table rows for a day
+		var $dayTableRows = $('div#'+e+' tr'),
+		dayInfo = [];
+
+		$dayTableRows.each(function(i, e){
+			var $this = $(this),
+			type = $this.find('.course_type').text(),
+			food = $this.find('.course_description').text();
+			dayInfo.push({
+				type: type,
+				food: food
 			});
-
-		textNodes.each(function(i, el) {
-			var current = el.data.trim(),
-				// Get the first word of the current element (May be "Fredag STÄNGT")
-				currentFirst = current.split(' ')[0],
-				header = 0;
-
-			// Check if this word should be considered a header
-			if (headers.indexOf(currentFirst) >= 0) {
-				header = 1;
-			}
-
-			finalLunches.push({header: header, text: current});
 		});
 
-		// Send the response
-		res.send(finalLunches);
+		weeklyInfo.push({
+			day: e,
+			info: dayInfo
+		});
 	});
-});
+
+	return weeklyInfo;	
+}
+
+var getLunches = function(req, res) {
+	request(edison_options, function (error, response, body) {
+		
+		var weeklyInfo = parseLunches(body);
+
+		// Send the response
+		res.send(weeklyInfo);
+	});
+};
+
+app.get('/get_lunches', getLunches);
 
 // Start the server
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
